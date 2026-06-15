141092d5b1f0b2842733bab79fb28d841635680c
emilia <emiliaritter@gmx.de>
2026-06-15 21:09:21 +0200

    löschen zugriff items open

--- chatbot_write_structured.py
+++ chatbot_write_structured.py
@@ -242,7 +242,6 @@ WEITERE INTERVIEW-REGELN:
 LEITFADEN:
 {TSDI_BESCHREIBUNGEN} 
 
-{TSDI_ITEMS}
 
 DEINE ANTWORT-STRUKTUR:
 Du musst deine Antwort zwingend als ein valides JSON-Objekt formatieren. Das JSON-Objekt muss exakt diese zwei Felder enthalten:
